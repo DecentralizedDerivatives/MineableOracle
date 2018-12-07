76569b55d665c1f5ca129bc4a7a0794850a11150
themandalore <fett.nicholas@yahoo.com>
2018-12-07 10:54:09 -0500

    Update miner.py

--- miner/miner.py
+++ miner/miner.py
@@ -2,19 +2,16 @@ import web3,json
 from web3 import Web3
 import requests,json, time,random
 import pandas as pd
+import hashlib
 from Naked.toolshed.shell import execute_js, muterun_js, run_js
-from multiprocessing import Process, freeze_support
-from struct import *
-contract_addresses = ["0x7a59bcbaf3bc4316c70338215446fd7a0456252c","0xc37b5b861bfc1a96f3ecef106392db42711d5c4d"];#1day and 10 min
+
+contract_addresses = ["0x72689F0E911adF8fc41200F0fb112D2D5472c6C3","0x00285C75fa033EBAd57f2267aEBF73db1f2Aae45"];#1day and 10 min
 node_url ="https://rinkeby.infura.io/"
 net_id = 4 #eth network ID
 last_block = 0
 
 public_keys = ["0xe010ac6e0248790e08f42d5f697160dedf97e024","0xcdd8fa31af8475574b8909f135d510579a8087d3","0xb9dd5afd86547df817da2d0fb89334a6f8edd891","0x230570cd052f40e14c14a81038c6f3aa685d712b","0x3233afa02644ccd048587f8ba6e99b3c00a34dcc"]
 private_keys = ["3a10b4bc1258e8bfefb95b498fb8c0f0cd6964a811eabca87df5630bcacd7216","d32132133e03be292495035cf32e0e2ce0227728ff7ec4ef5d47ec95097ceeed","d13dc98a245bd29193d5b41203a1d3a4ae564257d60e00d6f68d120ef6b796c5","4beaa6653cdcacc36e3c400ce286f2aefd59e2642c2f7f29804708a434dd7dbe","78c1c7e40057ea22a36a0185380ce04ba4f333919d1c5e2effaf0ae8d6431f14"]
-
-static_jazz1 = "0000000000000000000000000000000000000000000000000000000000000040000000000000000000000000"
-static_jazz2 = "157465737441646470726f706f7365644f7261636c65"
 
 def generate_random_number():
     return str(random.randint(1000000,9999999))
@@ -24,16 +21,26 @@ def mine(challenge, public_address, difficulty):
 	while True:
 		x += 1;
 		nonce = Web3.toHex(str.encode(str(generate_random_number())))
-		_string = str(challenge[0:]).strip() + public_address[2:].strip() + nonce[2:].strip()
-		n = Web3.sha3(hexstr = _string.strip())
-		hash1 = int.from_bytes(n, byteorder='big')
+		_string = str(challenge[1:]).strip() + public_address[2:].strip() + nonce[2:].strip()
+		_solution = Web3.toHex(Web3.sha3(text=_string.strip()))
+		rem = ( int(_solution,16) % 3)
+		if(rem == 2):
+			n = Web3.toHex(Web3.sha3(hexstr=_solution))
+		elif(rem == 1):
+			n = "0x" + hashlib.new('sha256',bytes.fromhex(_solution[2:])).hexdigest()
+		else:
+			n = Web3.toHex(Web3.sha3(hexstr=(hashlib.new('ripemd160',bytes.fromhex(_solution[2:])).hexdigest())));
+		print("n",n)
+		hash1 = int(n,16)
 		if hash1 % difficulty == 0:
 			return int(nonce,16);
 		if x % 10000 == 0:
-			_challenge,_difficulty = getVariables();
-			if _challenge == challenge:
-				pass;
-			else:
+			payload = {"jsonrpc":"2.0","id":net_id,"method":"eth_blockNumber"}
+			r = requests.post(node_url, data=json.dumps(payload));
+			d = jsonParser(r);
+			last_block = int(d['result'],16)
+			_challenge,_difficulty,v = getVariables();
+			if (v == True):
 				return 0;
 
 def getAPIvalue():
@@ -80,17 +87,6 @@ def jsonParser(_info):
 	data = json.loads(my_json)
 	s = json.dumps(data, indent=4, sort_keys=True)
 	return json.loads(s)
-
-def bytes2int(str):
- return int(str.encode('hex'), 32)
-
-def bytes_to_int(bytes):
-    result = 0
-
-    for b in bytes:
-        result = result * 256 + int(b)
-
-    return result
 
 #working()
 #getVariables()
